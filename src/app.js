c368e7c51efee0bed1a0be5ce6b837c608eaddf1
sneelyg <santiagoneely@gmail.com>
2022-07-21 01:01:53 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,50 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let pronoun = ["the", "our", "some"];
+  let adj = ["awesome", "big", "shiny"];
+  let noun = ["dude", "oaktree", "caterpillar"];
+  let com = [".com", ".org", ".com.au", ".cl"];
+
+  let domain = "";
+  let dominios = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < com.length; l++) {
+          domain = pronoun[i] + adj[j] + noun[k] + com[l];
+          dominios.push(domain);
+          console.log(domain);
+        }
+      }
+    }
+  }
+
+  Document;
+
   console.log("Hello Rigo from the console!");
+
+  function makeUL(array) {
+    // Create the list element:
+    var list = document.createElement("p");
+
+    for (let i = 0; i < array.length; i++) {
+      //Create the list item:
+      var item = document.createElement("p");
+
+      // Set its contents:
+      item.appendChild(document.createTextNode(array[i]));
+
+      // Add it to the list:
+      list.appendChild(item);
+    }
+
+    // Finally, return the constructed list:
+    return list;
+  }
+
+  // Add the contents of options[0] to #foo:
+  document.getElementById("#lista").appendChild(makeUL(dominios));
 };
